04d8838395e819354bf2acdeb11a0445ab556857
redcodemohammed <redcodemohammed@gmail.com>
2020-05-07 00:52:24 +0300

    added try catch

--- src/commands/tag.ts
+++ src/commands/tag.ts
@@ -2,11 +2,16 @@ import { TelegrafContext } from "telegraf/typings/context"
 import userModel from "../models/User";
 
 export default async (ctx: TelegrafContext) => {
-    let groupId = ctx.message.chat.id
-    let users = await userModel.find({ group: String(groupId) });
+    try {
 
-    if (users.length > 0) {
-        return ctx.reply(`${users.map(u => `${u.username}`).join(" ")}`);
+        let groupId = ctx.message.chat.id
+        let users = await userModel.find({ group: String(groupId) });
+
+        if (users.length > 0) {
+            return ctx.reply(`${users.map(u => `${u.username}`).join(" ")}`);
+        }
+        ctx.reply("No users yet, try /help to know how to add them");
+    } catch (err) {
+        console.log(err);
     }
-    ctx.reply("No users yet, try /help to know how to add them");
 };
