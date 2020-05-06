cb6c49aed3147451619cabf76b7f4afe45e21f7a
redcodemohammed <redcodemohammed@gmail.com>
2020-05-07 00:34:14 +0300

    fixed @ bug

--- src/commands/tag.ts
+++ src/commands/tag.ts
@@ -6,7 +6,7 @@ export default async (ctx: TelegrafContext) => {
     let users = await userModel.find({ group: String(groupId) });
 
     if (users.length > 0) {
-        return ctx.reply(`${users.map(u => `@${u.username}`).join(" ")}`);
+        return ctx.reply(`${users.map(u => `${u.username}`).join(" ")}`);
     }
     ctx.reply("No users yet, try /help to know how to add them");
 };
