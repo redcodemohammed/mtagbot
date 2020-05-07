cece20653a90fcea59a7d5f1a9c2fc2433bcb2c7
redcodemohammed <redcodemohammed@gmail.com>
2020-05-08 00:25:23 +0300

    the bot will tell you if you are in the list

--- src/commands/everyone/addMe.ts
+++ src/commands/everyone/addMe.ts
@@ -1,13 +1,20 @@
 import { TelegrafContext } from "telegraf/typings/context"
 import { addUser } from "../../helpers/users";
+import userModel from "../../models/User";
 
 
 export default async (ctx: TelegrafContext) => {
     try {
         let groupId = ctx.message.chat.id;
         let username = ctx.message.from.username;
-        await addUser(`@${username}`, String(groupId));
-        ctx.reply("Guess what! you were added.");
+        // check if the user is there:
+        let exists = await userModel.findOne({ username: `@${username}`, group: String(groupId) });
+        if (!exists) {
+            await addUser(`@${username}`, String(groupId));
+            ctx.reply("Guess what! you were added.");
+        } else {
+            ctx.reply("You are already in the list.");
+        }
     } catch (err) {
         console.log(err);
     }
