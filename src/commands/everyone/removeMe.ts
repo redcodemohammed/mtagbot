b16b612b6fb09be7b689e4ff8e5dd619b3c4f592
redcodemohammed <redcodemohammed@gmail.com>
2020-05-08 00:30:01 +0300

    fixed adding user multiple  times

--- src/commands/everyone/removeMe.ts
+++ src/commands/everyone/removeMe.ts
@@ -1,13 +1,20 @@
 import { TelegrafContext } from "telegraf/typings/context"
 import { removeUser } from "../../helpers/users";
+import userModel from "../../models/User";
 
 
 export default async (ctx: TelegrafContext) => {
     try {
         let groupId = ctx.message.chat.id;
         let username = ctx.message.from.username;
-        await removeUser(`@${username}`, String(groupId));
-        ctx.reply("Guess what! you were removed.");
+        // check if the user is there:
+        let exists = await userModel.findOne({ username: `@${username}`, group: String(groupId) });
+        if (exists) {
+            await removeUser(`@${username}`, String(groupId));
+            ctx.reply("Guess what! you were removed.");
+        } else {
+            ctx.reply("You are not even in the list.");
+        }
     } catch (err) {
         console.log(err);
     }
