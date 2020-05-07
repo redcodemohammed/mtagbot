9cff6054fb8cf1baed2f8fd75f4ef2c8ce5ccc75
redcodemohammed <redcodemohammed@gmail.com>
2020-05-08 01:30:18 +0300

    fixed a bug

--- src/commands/owner/spam.ts
+++ src/commands/owner/spam.ts
@@ -6,25 +6,30 @@ export default async (ctx: TelegrafContext) => {
     try {
         if (ctx.message.from.id === +process.env.ownerId) {
             let times = +ctx.message.text.split(" ")[1];
+            if (!times) {
+                ctx.reply("Please specify how many times to spam. /spam <n>")
+            } else {
 
-            let timer = 1;
-            let interval = setInterval(async () => {
-                if (timer === times) clearInterval(interval);
+                let timer = 1;
+                let interval = setInterval(async () => {
+                    if (timer === times) clearInterval(interval);
 
-                let groupId = ctx.message.chat.id
-                let users = await userModel.find({ group: String(groupId) });
+                    let groupId = ctx.message.chat.id
+                    let users = await userModel.find({ group: String(groupId) });
 
-                if (users.length > 0) {
-                    ctx.reply(`${users.map(u => `${u.username}`).join(" ")}`);
-                } else {
-                    clearInterval(interval);
-                    ctx.reply("No users yet, try /help to know how to add them");
-                }
-                timer++;
-            }, 1000);
+                    if (users.length > 0) {
+                        ctx.reply(`${users.map(u => `${u.username}`).join(" ")}`);
+                    } else {
+                        clearInterval(interval);
+                        ctx.reply("No users yet, try /help to know how to add them");
+                    }
+                    timer++;
+                }, 1000);
+            }
         } else {
             ctx.reply("You are not the owner.");
         }
+
     } catch (err) {
         console.log(err);
     }
