550da6f4daad5cd4bff727021d17db6e9e933b85
redcodemohammed <redcodemohammed@gmail.com>
2020-05-07 22:19:38 +0300

    fixed a bug

--- src/commands/handelNewGames.ts
+++ src/commands/handelNewGames.ts
@@ -2,7 +2,7 @@ import { TelegrafContext } from "telegraf/typings/context"
 import userModel from "../models/User";
 
 export default async (ctx: TelegrafContext, next: any) => {
-    if (ctx.update.message.text.includes("/startgame")) {
+    if (ctx?.update?.message?.text?.includes("/startgame")) {
 
         let group_id = ctx.message.chat.id;
         let users = await userModel.find({ group: String(group_id) });
