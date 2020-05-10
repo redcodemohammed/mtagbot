b557c4d82adcd6387338abecd0fe41d33321aba0
redcodemohammed <redcodemohammed@gmail.com>
2020-05-10 03:16:43 +0300

    added @admins command

--- src/commands/bot/autoTag.ts
+++ src/commands/bot/autoTag.ts
@@ -8,16 +8,40 @@ const tagOn = [
 ]
 
 export default async (ctx: TelegrafContext, next: any) => {
-    let text = ctx?.update?.message?.text;
-    let test = tagOn.find(el => text?.startsWith(el)) !== undefined
-    if (test) {
-        let group_id = ctx.message.chat.id;
-        let users = await userModel.find({ group: String(group_id) });
+    if (["group", "supergroup"].includes(ctx.chat.type)) {
+        let text = ctx?.update?.message?.text;
+        let test = text?.startsWith("@admin");
+        if (test) {
+            let admins = (await ctx.getChatAdministrators())
+                .map(admin => `@${admin.user.username}`)
+                .filter(admin => !admin.endsWith("bot"));
 
-        if (users.length > 0) {
-            ctx.reply(users.map(u => u.username).join(" "));
+            if (admins.length > 0) {
+                ctx.reply(admins.join(" "));
+            }
+        } else if (tagOn.find(el => text?.startsWith(el)) !== undefined) {
+            let group_id = ctx.message.chat.id;
+            let users = await userModel.find({ group: String(group_id) });
+
+            if (users.length > 0) {
+                ctx.reply(users.map(u => u.username).join(" "));
+            }
         }
+        else {
+            next();
+        }
+
     } else {
         next();
     }
 }
+
+
+
+// let text = ctx?.update?.message?.text;
+// let test = tagOn.find(el => text?.startsWith(el)) !== undefined
+// if (test) {
+
+// } else {
+//     next();
+// }
